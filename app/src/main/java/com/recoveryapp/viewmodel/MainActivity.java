660c5f82679dcf47cd5093beb835d9a39d0baf5d
piotreksaja <piotreksaja99@gmail.com>
2021-11-06 11:43:41 +0100

    Add colors and bottom navigation menu layout

--- app/src/main/java/com/recoveryapp/viewmodel/MainActivity.java
+++ app/src/main/java/com/recoveryapp/viewmodel/MainActivity.java
@@ -1,8 +1,10 @@
-package com.recoveryapp;
+package com.recoveryapp.viewmodel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.recoveryapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
 }
